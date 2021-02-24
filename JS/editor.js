83d4516e795b8a2644cd0b16c5d7d3750371b443
poormonitor <poormonitor@outlook.com>
2021-02-24 20:46:16 +0800

    UPDATE COMPRESSED FILE

--- JS/editor.js
+++ JS/editor.js
@@ -1,43 +1,4 @@
-function insertAtCursor(t, e) {
-	var n = t.scrollTop,
-	o = document.documentElement.scrollTop;
-	if (document.selection) {
-		t.focus();
-		var s = document.selection.createRange();
-		s.text = e,
-		s.select()
-	} else if (t.selectionStart || "0" == t.selectionStart) {
-		var l = t.selectionStart,
-		c = t.selectionEnd;
-		t.value = t.value.substring(0, l) + e + t.value.substring(c, t.value.length),
-		t.focus(),
-		t.selectionStart = l + e.length,
-		t.selectionEnd = l + e.length
-	} else t.value += e,
-	t.focus();
-	t.scrollTop = n,
-	document.documentElement.scrollTop = o
-}
-$(function() {
-
-	Smilies = {
-			dom: function(id) {
-					return document.getElementById(id);
-			},
-			grin: function(tag) {
-					tag = " "  + tag + " ";
-					myField = this.dom("text");
-					document.selection ? (myField.focus(), sel = document.selection.createRange(), sel.text = tag, myField.focus()) : this.insertTag(tag);
-			},
-			insertTag: function(tag) {
-					myField = Smilies.dom("text");
-					myField.selectionStart || myField.selectionStart == "0" ? (startPos = myField.selectionStart, endPos = myField.selectionEnd, cursorPos = startPos, myField.value = myField.value.substring(0, startPos) + tag + myField.value.substring(endPos, myField.value.length), cursorPos += tag.length, myField.focus(), myField.selectionStart = cursorPos, myField.selectionEnd = cursorPos) : (myField.value += tag, myField.focus());
-			}
-	}
-
-	$("#wmd-button-row").append('<li class="wmd-spacer wmd-spacer1"></li><li class="wmd-button" id="wmd-sc-button" style="" title="插入短代码">短代码</li>');
-	$("#wmd-button-row").append('<li class="wmd-spacer wmd-spacer1"></li><li class="wmd-button" id="wmd-owo-button" style="" title="插入表情">OWO</span></li>');
-	$("#wmd-button-row").append(`
+function insertAtCursor(n,t){var f=n.scrollTop,e=document.documentElement.scrollTop,r,i,u;document.selection?(n.focus(),r=document.selection.createRange(),r.text=t,r.select()):n.selectionStart||"0"==n.selectionStart?(i=n.selectionStart,u=n.selectionEnd,n.value=n.value.substring(0,i)+t+n.value.substring(u,n.value.length),n.focus(),n.selectionStart=i+t.length,n.selectionEnd=i+t.length):(n.value+=t,n.focus());n.scrollTop=f;document.documentElement.scrollTop=e}$(function(){function n(){$("#OwO-editor").css("display")=="none"?$("#OwO-editor").slideDown():$("#OwO-editor").slideUp()}function t(){$("#g-shortcode").css("display")=="none"?$("#g-shortcode").slideDown():$("#g-shortcode").slideUp()}Smilies={dom:function(n){return document.getElementById(n)},grin:function(n){n=" "+n+" ";myField=this.dom("text");document.selection?(myField.focus(),sel=document.selection.createRange(),sel.text=n,myField.focus()):this.insertTag(n)},insertTag:function(n){myField=Smilies.dom("text");myField.selectionStart||myField.selectionStart=="0"?(startPos=myField.selectionStart,endPos=myField.selectionEnd,cursorPos=startPos,myField.value=myField.value.substring(0,startPos)+n+myField.value.substring(endPos,myField.value.length),cursorPos+=n.length,myField.focus(),myField.selectionStart=cursorPos,myField.selectionEnd=cursorPos):(myField.value+=n,myField.focus())}};$("#wmd-button-row").append('<li class="wmd-spacer wmd-spacer1"><\/li><li class="wmd-button" id="wmd-sc-button" style="" title="插入短代码">短代码<\/li>');$("#wmd-button-row").append('<li class="wmd-spacer wmd-spacer1"><\/li><li class="wmd-button" id="wmd-owo-button" style="" title="插入表情">OWO<\/span><\/li>');$("#wmd-button-row").append(`
 		<div id="OwO-editor" style="display:none"><div class="OwO OwO-open" id="qaq">
 	   <div class="OwO-body" id="OwO-body">
 	    <ul id="OwO-pp" class="OwO-items OwO-items-emoticon OwO-items-show" style="max-height: 197px;">
@@ -127,8 +88,7 @@ $(function() {
 	    </ul>
 	   </div>
 	  </div>
-</div>`);
-	$("#wmd-button-row").append(`
+</div>`);$("#wmd-button-row").append(`
 		<div id="g-shortcode" style="display:none">
 			<a id="g-video">视频</a>
 			<a id="g-collapse">折叠框</a>
@@ -142,85 +102,4 @@ $(function() {
 			<a id="g-tag">标签</a>
 			<a id="g-photos">相册</a>
 	</div>
-		`);
-
-	function OwO_show(){
-		if($("#OwO-editor").css("display")=='none'){
-			 $("#OwO-editor").slideDown();
-		}else{
-			 $("#OwO-editor").slideUp();
-		 }
-	}
-	function sc_show(){
-		if($("#g-shortcode").css("display")=='none'){
-			 $("#g-shortcode").slideDown();
-		}else{
-			 $("#g-shortcode").slideUp();
-		 }
-	}
-
-	$(document).on("click", "#g-video",
-	function() {
-		myField = document.getElementById("text"),
-		insertAtCursor(myField, '\n\n[dplayer id="" url="" pic=""]\n\n[/dplayer]\n\n')
-	});
-
-	$(document).on("click", "#g-collapse",
-	function() {
-		myField = document.getElementById("text"),
-		insertAtCursor(myField, '\n\n[collapse title=""]\n\n[/collapse]')
-	});
-
-	$(document).on("click", "#g-art",
-	function() {
-		myField = document.getElementById("text"),
-		insertAtCursor(myField, '[art]文章ID[/art]')
-	});
-	$(document).on("click", "#g-bili",
-	function() {
-		myField = document.getElementById("text"),
-		insertAtCursor(myField, '[bili]BV/AV[/bili]')
-	});
-	$(document).on("click", "#g-notice",
-	function() {
-		myField = document.getElementById("text"),
-		insertAtCursor(myField, '[notice]内容[/notice]')
-	});
-	$(document).on("click", "#g-notice-block",
-	function() {
-		myField = document.getElementById("text"),
-		insertAtCursor(myField, '[notice-block]内容[/notice-block]')
-	});
-	$(document).on("click", "#g-warn-block",
-	function() {
-		myField = document.getElementById("text"),
-		insertAtCursor(myField, '[warn-block]内容[/warn-block]')
-	});
-	$(document).on("click", "#g-warn",
-	function() {
-		myField = document.getElementById("text"),
-		insertAtCursor(myField, '[warn]内容[/warn]')
-	});
-	$(document).on("click", "#g-dl",
-	function() {
-		myField = document.getElementById("text"),
-		insertAtCursor(myField, '[dl href="链接（无需http://）"]文件名[/dl]')
-	});
-	$(document).on("click", "#g-tag",
-	function() {
-		myField = document.getElementById("text"),
-		insertAtCursor(myField, '[tag]内容[/tag]')
-	});
-	$(document).on("click", "#g-photos",
-	function() {
-		myField = document.getElementById("text"),
-		insertAtCursor(myField, '\n[photos]\n图片1描述,图片1链接|\n图片2描述,图片2链接|\n以此类推，以 | 符号分割\n[/photos]\n')
-	});
-
-	$(document).on("click","#wmd-owo-button",function(){OwO_show();});
-	$(document).on("click","#wmd-sc-button",function(){sc_show();});
-
-
-
-
-});
+		`);$(document).on("click","#g-video",function(){myField=document.getElementById("text");insertAtCursor(myField,'\n\n[dplayer id="" url="" pic=""]\n\n[/dplayer]\n\n')});$(document).on("click","#g-collapse",function(){myField=document.getElementById("text");insertAtCursor(myField,'\n\n[collapse title=""]\n\n[/collapse]')});$(document).on("click","#g-art",function(){myField=document.getElementById("text");insertAtCursor(myField,"[art]文章ID[/art]")});$(document).on("click","#g-bili",function(){myField=document.getElementById("text");insertAtCursor(myField,"[bili]BV/AV[/bili]")});$(document).on("click","#g-notice",function(){myField=document.getElementById("text");insertAtCursor(myField,"[notice]内容[/notice]")});$(document).on("click","#g-notice-block",function(){myField=document.getElementById("text");insertAtCursor(myField,"[notice-block]内容[/notice-block]")});$(document).on("click","#g-warn-block",function(){myField=document.getElementById("text");insertAtCursor(myField,"[warn-block]内容[/warn-block]")});$(document).on("click","#g-warn",function(){myField=document.getElementById("text");insertAtCursor(myField,"[warn]内容[/warn]")});$(document).on("click","#g-dl",function(){myField=document.getElementById("text");insertAtCursor(myField,'[dl href="链接（无需http://）"]文件名[/dl]')});$(document).on("click","#g-tag",function(){myField=document.getElementById("text");insertAtCursor(myField,"[tag]内容[/tag]")});$(document).on("click","#g-photos",function(){myField=document.getElementById("text");insertAtCursor(myField,"\n[photos]\n图片1描述,图片1链接|\n图片2描述,图片2链接|\n以此类推，以 | 符号分割\n[/photos]\n")});$(document).on("click","#wmd-owo-button",function(){n()});$(document).on("click","#wmd-sc-button",function(){t()})})
